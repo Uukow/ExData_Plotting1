f269ff4894707df7ddf18938fd068c60027696ba
Uukow <93025470+Uukow@users.noreply.github.com>
2026-06-11 11:49:24 +0300

    Add plot4.R for 2x2 panel plot of power consumption
    This script downloads and processes household power consumption data to create a 2x2 panel plot of various metrics over time.

---
+++ plot4.R
@@ -0,0 +1,16 @@
+# plot4.R - 2x2 Panel Plot
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "household_power_consumption.zip")
+unzip("household_power_consumption.zip")
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+sub_df <- df[df$Date %in% c("1/2/2007","2/2/2007"),]
+sub_df$datetime <- strptime(paste(sub_df$Date, sub_df$Time), "%d/%m/%Y %H:%M:%S")
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(sub_df$datetime, sub_df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+plot(sub_df$datetime, sub_df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(sub_df$datetime, sub_df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(sub_df$datetime, sub_df$Sub_metering_2, col="red")
+lines(sub_df$datetime, sub_df$Sub_metering_3, col="blue")
+legend("topright", lty=1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.7, bty="n")
+plot(sub_df$datetime, sub_df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
